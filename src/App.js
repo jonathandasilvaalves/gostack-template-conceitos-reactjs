a47947904a9b5a79c07a34d5b88f34fdd8b9fc9e
Jonathan da Silva Alves <35965966+jonathandasilvaalves@users.noreply.github.com>
2020-07-27 12:46:27 -0300

    Update App.js
    Implementando métodos de adicionar e remover.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+  async function loadRepository() {
+    const response = await api.get("/repositories");
+    setRepositories(response.data);
+  }
+  useEffect(() => {
+    loadRepository();
+  }, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: "Teste",
+      url: 'http://jonathan.com.br', 
+      techs: ['React', 'Nodejs'],
+    });
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+    setRepositories([...repositories, response.data]);
+  }
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter((repo) => repo.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repositorie) => (
+          <li key={repositorie.id}>
+            {repositorie.title}
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
 }
 
 export default App;
